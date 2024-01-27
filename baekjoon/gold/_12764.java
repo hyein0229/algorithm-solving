d89fc4cbd556fbb2378e590b56e9878b1eb8acb0
hyein0229 <85734016+hyein0229@users.noreply.github.com>
2024-01-27 15:02:41 +0900

    boj: 12764 싸지방에 간 준하

--- baekjoon/gold/_12764.java
+++ baekjoon/gold/_12764.java
@@ -6,55 +6,78 @@ import java.util.*;
 문제: 12764 싸지방에 간 준하
 난이도: Gold 3
 알고리즘: 구현, 자료 구조, 시뮬레이션, 우선순위 큐
-풀이 방법: 사람이 빨리 들어오는 대로 컴퓨터를 차지하므로 우선순위 큐를 이용하여 시작 시각을 오름차순으로 정렬한 뒤
-        한 명씩 꺼내면서 컴퓨터를 배치해주어야 겠다고 생각했다. 각 컴퓨터 사용 종료 시각을 저장하기 위해 배열을 선언하고
-        배열을 앞에서부터 순회하면서 종료 시각보다 현재 사람의 시작 시각이 크다면 컴퓨터가 사용 시각이 겹치지 않는 것이므로
-        해당 컴퓨터에 그 사람을 배치시키고 종료 시각을 갱신해주었다.
-        만약 이때 컴퓨터에 배치하지 못할 경우 새로운 컴퓨터를 증설한다. 이때, X의 값을 증가시킨다.
+풀이 방법: 우선순위 큐를 3개 사용한다.
+       1. 대기하는 사용자 큐 -> 시작 시각이 빠른 순 기준
+       2. 사용 중인 컴퓨터 큐 -> 종료 시각이 빠른 순 기준
+       3. 비어 있는 컴퓨터 큐 -> 컴퓨터 번호가 작은 기준
+       사용자 큐에서 사용자를 꺼낸 다음 사용 중인 컴퓨터 큐에서 현재 사용자의 시작 시각보다 종료 시각이 더 작은
+       사용 가능한 컴퓨터들을 찾아 비어 있는 컴퓨터 큐에 삽입한다.
+       그 다음 비어 있는 컴퓨터 큐가 empty 라면 사용 가능한 컴퓨터가 없다는 것이므로 X값을 늘려 컴퓨터를 증설한다.
+       empty 가 아니면 큐에서 꺼낸 컴퓨터 번호가 사용자가 사용할 컴퓨터 번호가 된다.
  */
 public class _12764 {
+
+    static class User {
+        int start;
+        int end;
+
+        public User(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<int[]> Q = new PriorityQueue<>(new Comparator<int[]>() {
+        PriorityQueue<User> users = new PriorityQueue<>(Comparator.comparing(user -> user.start)); // 사용자 시작 시각 빠른 순
+        PriorityQueue<int[]> usingComs = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0]; // 시작 시각 오름차순
+                return o1[1] - o2[1]; // 종료 시각 빠른 순
             }
         });
+        PriorityQueue<Integer> emptyComs = new PriorityQueue<>(); // 컴퓨터 번호 작은 순
 
         for(int i=0; i<N; i++) {
             String[] line = br.readLine().split(" ");
-            Q.add(new int[]{Integer.parseInt(line[0]), Integer.parseInt(line[1])});
+            int start = Integer.parseInt(line[0]);
+            int end = Integer.parseInt(line[1]);
+            users.add(new User(start, end));
         }
 
-        int[] useCnt = new int[N];
-        int[] computers = new int[N];
         int X = 0; // 컴퓨터 개수
-        while(!Q.isEmpty()) {
-            int[] person = Q.poll();
-
-            boolean flag = false;
-            for(int i=0; i<X; i++) {
-                if(computers[i] <= person[0]) {
-                    computers[i] = person[1];
-                    useCnt[i]++;
-                    flag = true;
+        int[] useCnt = new int[N+1];
+        while(!users.isEmpty()) {
+            User user = users.poll();
+            // 지금 사용할 수 있는 컴퓨터 찾기
+            while(!usingComs.isEmpty()) {
+                int[] com = usingComs.peek();
+                int num = com[0];
+                int endTime = com[1];
+                if(endTime <= user.start) {
+                    emptyComs.add(num);
+                    usingComs.poll();
+                } else {
                     break;
                 }
             }
-            // 컴퓨터 증설
-            if(!flag) {
-                computers[X] = person[1];
+
+            // 비어있는 컴퓨터가 없다면 컴퓨터 증설
+            if(emptyComs.isEmpty()) {
+                usingComs.add(new int[]{++X, user.end});
                 useCnt[X]++;
-                X++;
+
+            } else {
+                int num = emptyComs.poll();
+                useCnt[num]++;
+                usingComs.add(new int[]{num, user.end});
             }
         }
 
         System.out.println(X);
-        for(int i=0; i<X; i++) {
+        for(int i=1; i<=X; i++) {
             System.out.print(useCnt[i] + " ");
         }
     }
